c47ee036a876c89b19bd00115ff38cd2a0e62aaa
Ester <ester.caroline.1009@gmail.com>
2022-04-29 15:50:50 -0300

    Versao1

---
+++ src/br/com/dio/Main.java
@@ -0,0 +1,25 @@
+package br.com.dio;
+
+public class Main {
+    public static void main(String[] args){
+        //Calculadora
+        System.out.println("Exercicio Calculadora");
+        Calculadora.soma(3, 6);
+        Calculadora.subtracao(9, 1.8);
+        Calculadora.multiplicacao(7,8);
+        Calculadora.divisao(5,2.5);
+        //Mensagem
+        System.out.println("Exercicio Mensagem");
+        Mensagem.obterMensagem(9);
+        Mensagem.obterMensagem(14);
+        Mensagem.obterMensagem(1);
+        //Emprestimo
+        System.out.println("Exercicio emprestimo");
+        Emprestimo.calcular(1000,Emprestimo.getDuasParcelas());
+        Emprestimo.calcular(1000,Emprestimo.getTresParcelas());
+        Emprestimo.calcular(1000,2);
+        Emprestimo.calcular(1000,3);
+        Emprestimo.calcular(1000,5);
+
+    }
+}
